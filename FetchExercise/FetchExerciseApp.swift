2a0ea234b03c2e843743e68f4d8a171011a5c0b3
ltran39 <ltran39@uic.edu>
2023-08-28 13:18:30 -0500

    Initial Commit

---
+++ FetchExercise/FetchExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchExerciseApp.swift
+//  FetchExercise
+//
+//  Created by Long Tran M2 on 28/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
